ee0c719aaf9830fd8bb542bc7de8a60954955b67
Nikolay Lembinen <homafromptz@gmail.com>
2016-06-13 18:00:57 +0300

    Demo preloader and flipper

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,29 @@
 (function($) {
 	'use strict';
 
-	$('#scene').parallax({
+	$("#scene").parallax({
 		scalarX: 3,
 		scalarY: 3,
 		frictionX: 0.5,
 		frictionY: 0.5
 	});
+
+	$(".welcome-page .login-button").click(function() {
+		$("body").addClass("welcome-page_flipped");
+	});
+
+	$(window).click(function(e) {
+		$("body").removeClass("welcome-page_flipped");
+	});
+
+	$(".welcome-page .flip-card, .welcome-page .login-button").click(function(e){
+		e.stopPropagation();
+	});
+
+	setTimeout(function(){
+		$(".preloader")
+			.fadeOut(400, function(){
+				$(".flip-card").addClass("loaded");
+			});
+	}, 1000);
 })(jQuery);
