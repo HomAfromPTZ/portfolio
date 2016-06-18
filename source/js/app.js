b41f354d8dac5712b27d82b720094618d40fc965
Nikolay Lembinen <homafromptz@gmail.com>
2016-06-18 19:06:50 +0300

    Footer

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,9 @@
 (function($) {
 	"use strict";
 
+	// ==============================
+	// Axis Parallax
+	// ==============================
 	$("#scene").parallax({
 		scalarX: 3,
 		scalarY: 3,
@@ -8,6 +11,10 @@
 		frictionY: 0.5
 	});
 
+
+	// ==============================
+	// Login card flip
+	// ==============================
 	$(".login-button").click(function() {
 		$("body").addClass("card_flipped");
 	});
@@ -16,6 +23,11 @@
 		$("body").removeClass("card_flipped");
 	});
 
+
+
+	// ==============================
+	// Main menu
+	// ==============================
 	$("#menu-toggle").click(function(){
 		$(this).add(".main-menu").toggleClass("active");
 	});
@@ -24,10 +36,60 @@
 		$(this).css("transition-delay", 0.3+0.1*index + "s");
 	});
 
+
+
+	// ==============================
+	// Fake preloader
+	// ==============================
 	setTimeout(function(){
 		$(".preloader")
 			.fadeOut(400, function(){
 				$(".flip-card").addClass("loaded");
 			});
 	}, 500);
+
+
+	// ==============================
+	// Testimonials section bg size
+	// ==============================
+	if( $(window).width()>1200){
+		$(".talks, .contact-form__bg").css("background-size", $(window).width() + "px");
+	}
+
+
+
+	// ==============================
+	// Contact form blur position
+	// ==============================
+/*	var talks_offset = $("section.talks").offset(),
+		cform_offset = $(".contact-form__bg").offset();
+
+	$(".contact-form__bg").css("background-position", "center -" + (cform_offset.top - talks_offset.top) +"px");
+*/
+
+	// ==============================
+	// SCROLL EVENTS
+	// ==============================
+	// $(window).scroll(function() {
+	// 	// var scrollPos = $(this).scrollTop();
+	// });
+
+
+
+	// ==============================
+	// RESIZE EVENTS
+	// ==============================
+	$(window).resize(function() {
+
+		// Testimonials section bg size
+		if( $(window).width()>1200){
+			$(".talks, .contact-form__bg").css("background-size", $(window).width() + "px");
+		}
+
+
+		// Contact form blur position
+		/*talks_offset = $("section.talks").offset();
+		cform_offset = $(".contact-form__bg").offset();
+		$(".contact-form__bg").css("background-position", "center -" + (cform_offset.top - talks_offset.top) +"px");*/
+	});
 })(jQuery);
