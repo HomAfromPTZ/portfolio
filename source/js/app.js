2a2bdd0f9ef5ccb527980c4860e17bc4ff00bed3
Nikolay Lembinen <homafromptz@gmail.com>
2016-06-19 16:14:33 +0300

    Thats all

--- source/js/app.js
+++ source/js/app.js
@@ -20,29 +20,9 @@
 		});
 	};
 
-	$("header .svg-heading, talks .svg-heading .testimonial, .portfolio-slider__module>div, .about-me__skills>div").animated("fadeInUp");
+	$("header .svg-heading, .talks .svg-heading, .talks .testimonial, .portfolio-slider__module>div, .about-me__skills>div").animated("fadeInUp");
+	$(".article").animated("fadeIn");
 
-
-
-	// ==============================
-	// Equal Heights
-	// ==============================
-	$.fn.equalHeights = function() {
-		var maxHeight = 0,
-			$this = $(this);
-		$this.each( function() {
-			var height = $(this).height();
-			if ( height > maxHeight ) {
-				maxHeight = height;
-			}
-		});
-
-		return $this.css("height", maxHeight);
-	};
-
-	setTimeout(function(){
-		$(".about-me>div>div").equalHeights();
-	}, 200);
 
 	// ==============================
 	// Axis Parallax
@@ -96,7 +76,9 @@
 		}, 700, "swing");
 	});
 
-
+	$(".blog-navigation__toggle").click(function(){
+		$(".blog-navigation").toggleClass("active");
+	});
 
 	// ==============================
 	// Fake preloader
@@ -109,7 +91,7 @@
 	}, 500);
 
 	// Page change
-	$(document).on("click", "a", function(e) {
+	$(document).on("click", "a.preload-link", function(e) {
 		var href = $(this).attr("href");
 		e.preventDefault();
 
@@ -119,40 +101,74 @@
 			});
 	});
 
-	// ==============================
-	// Testimonials section bg size
-	// ==============================
-	// if( $(window).width()>2000){
-	// 	$(".talks, .contact-form__bg").css("background-size", $(window).width() + "px");
-	// }
-
-
-
-	// ==============================
-	// Contact form blur position
-	// ==============================
-	// document.addEventListener("DOMContentLoaded", blurPosition);
-	// function blurPosition(){
-	// 	var talks_offset = $("section.talks").offset(),
-	// 		cform_offset = $(".contact-form__bg").offset();
-
-	// 	$(".contact-form__bg").css("background-position", "center -" + (cform_offset.top - talks_offset.top) +"px");
-	// };
 
 	// ==============================
 	// SCROLL EVENTS
 	// ==============================
-	// $(window).scroll(function() {
-	// 	// var scrollPos = $(this).scrollTop();
-	// });
 
+	// SCROLL NAVIGATION BEGIN
+	var lastId,
+		menu = $(".blog-navigation"),
+		menuItems = menu.find("li a"),
+		scrollItems = menuItems.map(function(){
+			var item = $($(this).attr("href"));
+			if (item.length) return item;
+		});
+
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+		var href = $(this).attr("href"),
+			offsetTop = (href === "#") ? 0 : $(href).offset().top-60;
+		
+		$("html, body").stop().animate({ 
+			scrollTop: offsetTop
+		}, 300);
+		e.preventDefault();
+	});
+
+	// Bind to scroll
+
+	$(window).scroll(function() {
+		// Get container scroll position
+		var fromTop = $(this).scrollTop(),
+			blogNavOffset = $(".blog-navigation").offset().top,
+			blogNavLimit = $(".footer__wrapper").offset().top - $(".blog-navigation__wrapper").outerHeight();
+
+		// Get id of current scroll item
+		var cur = scrollItems.map(function(){
+			if ($(this).offset().top < fromTop+144)
+				return this;
+		});
+		// Get the id of the current element
+		cur = cur[cur.length-1];
+		var id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+			.removeClass("active")
+			.filter("[href=#"+id+"]")
+			.addClass("active");
+		}
+
+		if(fromTop >= blogNavLimit && $(window).width()>768) {
+			$(".blog-navigation__wrapper").css({"position":"absolute", "top":blogNavLimit + "px"});
+		} else if (fromTop >= blogNavOffset && $(window).width()>768) {
+			$(".blog-navigation__wrapper").css({"position":"fixed", "top":0});
+			$(".blog-navigation__wrapper").addClass("nav-fixed");
+		} else {
+			$(".blog-navigation__wrapper").css({"position":"relative"});
+			$(".blog-navigation__wrapper").removeClass("nav-fixed");
+		}
+	});
+	// SCROLL NAVIGATION END
 
 
 	// ==============================
 	// RESIZE EVENTS
 	// ==============================
-
-
 	$(window).resize(function() {
 
 		// Testimonials section bg size
@@ -160,11 +176,5 @@
 			$(".talks, .contact-form__bg").css("background-size", $(window).width() + "px");
 		}
 
-		$(".about-me>div>div").css("height","auto").equalHeights();
-
-		// Contact form blur position
-		// var talks_offset = $("section.talks").offset(),
-		// 	cform_offset = $(".contact-form__bg").offset();
-		// $(".contact-form__bg").css("background-position", "center -" + (cform_offset.top - talks_offset.top) +"px");
 	});
 })(jQuery);
