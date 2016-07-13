11101c178a871406d4d51a6a09bd41d3b0bb7174
Nikolay Lembinen <homafromptz@gmail.com>
2016-07-13 10:04:39 +0300

    Complete Jade/Sass/Js/Mail bundle

--- gulp/tasks/sprites_svg.js
+++ gulp/tasks/sprites_svg.js
@@ -10,8 +10,8 @@ var svg_config = {
 		symbol : {
 			prefix : "%s",
 			sprite : "bg-sprite.svg",
-			render : { scss : true },
-			example : true
+			// render : { scss : true },
+			// example : true
 		}
 	}
 };
